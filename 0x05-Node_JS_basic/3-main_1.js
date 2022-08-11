f586d91e25419fdca3402d70e8fbcdfeebdcee83
root <ogegagideon9@gmail.com>
2022-08-11 09:20:14 +0000

    Added Files

--- 0x05-Node_JS_basic/3-main_1.js
+++ 0x05-Node_JS_basic/3-main_1.js
@@ -1,38 +1,10 @@
-/* eslint-disable */
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-const countStudents = (path) => {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (err, dataCollected) => {
-      if (err) rej(Error('Cannot load the database'));
-      const messages = [];
-      let message;
-      const content = dataCollected.toString().split('\n');
-      let students = content.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const nStudents = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${nStudents}`;
-      console.log(message);
-      messages.push(message);
-      const subjects = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-          subjects[students[i][3]].push(students[i][0]);
-        }
-      }
-      delete subjects.subject;
-      for (const key of Object.keys(subjects)) {
-        message = `Number of students in ${key}: ${
-          subjects[key].length
-        }. List: ${subjects[key].join(', ')}`;
-        console.log(message);
-        messages.push(message);
-      }
-      res(messages);
+countStudents("database.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
     });
-  };
-  return new Promise(promise);
-};
-
-module.exports = countStudents;
+console.log("After!");
